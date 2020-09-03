ad719267d221a2735c21d25783558f27b9827ccd
xScoopy <JLeary070@gmail.com>
2020-09-03 11:19:59 -0700

    finished tutorial

--- functions.py
+++ functions.py
@@ -14,3 +14,16 @@ result4 = subtract(10, 4)
 print(result4)
 result5 = subtract(9, 4)
 print(result5)
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def add(number1, number2):
+    result = number1 + number2
+    return result 
+
+sum1 = add(3, 4)
+print(sum1)
+sum2 = add(14, 32)
+print(sum2)
