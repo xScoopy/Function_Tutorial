e5e547784563a23c10665ee870f1f8ec3fc1cf20
xScoopy <JLeary070@gmail.com>
2020-09-03 11:12:51 -0700

    Q 1-5 done

---
+++ functions.py
@@ -0,0 +1,16 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+result2 = area(3, 6)
+result3 = area(7, 9)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result4 = subtract(10, 4)
+print(result4)
+result5 = subtract(9, 4)
+print(result5)
